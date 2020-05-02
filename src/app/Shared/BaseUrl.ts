7e5066d080eb7d44d25994f27ebcb02f00399b36
Alvaro Atias <aat@softfluent.com>
2020-05-02 19:35:56 +0200

    final modifications

--- src/app/Shared/BaseUrl.ts
+++ src/app/Shared/BaseUrl.ts
@@ -2,7 +2,9 @@ import { Global } from './Global';
 import { environment } from 'src/environments/environment';
 
 export class Urls {
+    
     public static TASK_GETALL = environment.url + Global.TASK_ENDPOINT + "GetAll";
     public static TASK_ADD = environment.url + Global.TASK_ENDPOINT + "Add";
     public static TASK_UPDATESTATUS = environment.url + Global.TASK_ENDPOINT + "UpdateStatus";
+    public static TASK_DELETE = environment.url + Global.TASK_ENDPOINT + "Delete?taskId=";
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -23,11 +23,16 @@ export class TaskService {
     return this._http.post<Task>(Urls.TASK_ADD, { "taskDescription": taskDescription }, httpOptions);
   }
 
+  DeleteTask(taskId: string) {
+    let url = Urls.TASK_DELETE + taskId;
+    return this._http.delete(url, httpOptions);
+  }
+
   GetAllTasks() : Observable<Task[]> {
     return this._http.get<Task[]>(Urls.TASK_GETALL);
   }
 
-  UpdateTaskStatus(taskId: string): Observable<Task> {
+  UpdateTaskStatus(taskId: string) : Observable<Task> {
     return this._http.put<Task>(Urls.TASK_UPDATESTATUS, { "taskId": taskId }, httpOptions);
   }
 }
